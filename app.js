baded55bb43c5a9428bddc15277edaeed63fa72a
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-08 06:30:53 +0600

    functionality added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function sequencer() {
-  const kick = new Tone.Player("./drums/kick-electro01.wav").toDestination();
-  const snare = new Tone.Player("./drums/snare-lofi02.wav").toDestination();
+  const kick = new Tone.Player("./drums/kick-electro01.wav").toMaster();
+  const snare = new Tone.Player("./drums/snare-lofi02.wav").toMaster();
   let index = 0;
 
   Tone.Transport.scheduleRepeat(repeat, "8n");
@@ -12,9 +12,14 @@ function sequencer() {
     let kickInputs = document.querySelector(
       `.kick input:nth-child(${step + 1})`
     );
+    let snareInputs = document.querySelector(
+      `.snare input:nth-child(${step + 1})`
+    );
     if (kickInputs.checked) {
-     
       kick.start();
+    }
+    if (snareInputs.checked) {
+      snare.start();
     }
     index++;
   }
